88b0e828355265d0bd550715662e9cf1c9c899f0
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-10-18 14:41:19 +0700

    bugs fixing

--- app/src/main/java/com/example/md_lab04__recyclerview_getrequests/MainActivity.kt
+++ app/src/main/java/com/example/md_lab04__recyclerview_getrequests/MainActivity.kt
@@ -7,12 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.net.HttpURLConnection
 import java.net.URL
-
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "Testing HTTPUrlConnection"
+    private val httpConnectionTag = "Flickr cats"
+    private val okHttpConnectionTag = "Flickr OkCats"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,11 +36,29 @@ class MainActivity : AppCompatActivity() {
         val getViaOkHttpButton: Button = findViewById(R.id.buttonOkHTTP)
 
         getViaHttpButton.setOnClickListener {
-            try {
-                val data = urlConnection.inputStream.bufferedReader().readText()
-                Log.d(TAG, data) // d - отладка (debug)
-            } finally {
-                urlConnection.disconnect()
+            thread {
+                try {
+                    val data = urlConnection.inputStream.bufferedReader().readText()
+                    Log.d(httpConnectionTag, data) // d - отладка (debug)
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }
+        }
+
+        getViaOkHttpButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val client = OkHttpClient()
+                val request = Request.Builder().url("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1").build()
+
+                try {
+                    val response: Response = client.newCall(request).execute()
+                    val data = response.body?.string()
+                    Log.i(okHttpConnectionTag, data?: "No response")
+                }
+                catch (e: Exception) {
+                    Log.e(okHttpConnectionTag,"Error: ${e.message}")
+                }
             }
         }
     }
